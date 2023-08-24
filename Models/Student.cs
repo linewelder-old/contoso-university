61efbbf0dbe09195a350e2364427fda172cf5b68
linewelder <redalerm@gmail.com>
2023-08-24 14:48:57 +0200

    Add models

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+namespace ContosoUniversity.Models;
+
+public class Student
+{
+    public int ID { get; set; }
+    public string LastName { get; set; } = null!;
+    public string FirstMidName { get; set; } = null!;
+    public DateTime EnrollmentDate { get; set; }
+
+    public ICollection<Enrollment> Enrollments { get; set; } = null!;
+}
