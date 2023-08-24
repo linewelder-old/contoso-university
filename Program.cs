2dc1b3fae5b3c20b4b9e15fefa0ab4991eddf063
linewelder <redalerm@gmail.com>
2023-08-24 15:04:55 +0200

    Add call to Database.EnsureCreated()

--- Program.cs
+++ Program.cs
@@ -26,6 +26,13 @@ else
     app.UseMigrationsEndPoint();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
