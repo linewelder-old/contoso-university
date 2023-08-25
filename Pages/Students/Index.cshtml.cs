ee9fff2f17677e6188768a16ac9e552df070a1de
linewelder <redalerm@gmail.com>
2023-08-25 19:15:36 +0200

    Add sorting to Students/Index

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -7,6 +7,14 @@ namespace ContosoUniversity.Pages.Students;
 
 public class IndexModel : PageModel
 {
+    public enum SortOrder
+    {
+        NameAsc,
+        NameDesc,
+        DateAsc,
+        DateDesc
+    }
+
     private readonly SchoolContext _context;
 
     public IndexModel(SchoolContext context)
@@ -16,11 +24,49 @@ public class IndexModel : PageModel
 
     public IList<Student> Students { get;set; } = default!;
 
-    public async Task OnGetAsync()
+    /// <summary>
+    /// Sorting parameter to be applied when name column header is pressed.
+    /// </summary>
+    public SortOrder NameNextSortOrder { get; set; }
+
+    /// <summary>
+    /// Sorting parameter to be applied when enrollment date column header is pressed.
+    /// </summary>
+    public SortOrder DateNextSortOrder { get; set; }
+
+    public SortOrder CurrentSortOrder { get; set; }
+
+    public async Task OnGetAsync(SortOrder? sortOrder)
     {
+        NameNextSortOrder = sortOrder == SortOrder.NameAsc
+            ? SortOrder.NameDesc : SortOrder.NameAsc;
+        DateNextSortOrder = sortOrder == SortOrder.DateAsc
+            ? SortOrder.DateDesc : SortOrder.DateAsc;
+        CurrentSortOrder = sortOrder ?? SortOrder.NameAsc;
+
+        IQueryable<Student> students = _context.Students;
+        switch (CurrentSortOrder)
+        {
+            case SortOrder.NameAsc:
+                students = students.OrderBy(s => s.LastName);
+                break;
+
+            case SortOrder.NameDesc:
+                students = students.OrderByDescending(s => s.LastName);
+                break;
+
+            case SortOrder.DateAsc:
+                students = students.OrderBy(s => s.EnrollmentDate);
+                break;
+
+            case SortOrder.DateDesc:
+                students = students.OrderByDescending(s => s.EnrollmentDate);
+                break;
+        }
+
         if (_context.Students != null)
         {
-            Students = await _context.Students.ToListAsync();
+            Students = await students.AsNoTracking().ToListAsync();
         }
     }
 }
