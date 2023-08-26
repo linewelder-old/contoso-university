559582020eb1c54436b12a815ba59d16b80f7050
linewelder <redalerm@gmail.com>
2023-08-26 10:28:02 +0200

    Add datatype date to Student.EnrollmentDate

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models;
 
 public class Student
@@ -5,6 +7,7 @@ public class Student
     public int ID { get; set; }
     public string LastName { get; set; } = null!;
     public string FirstMidName { get; set; } = null!;
+    [DataType(DataType.Date)]
     public DateTime EnrollmentDate { get; set; }
 
     public ICollection<Enrollment>? Enrollments { get; set; } = null;
--- Models/ViewModels/StudentVM.cs
+++ Models/ViewModels/StudentVM.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models.ViewModels;
 
 public class StudentVM
 {
     public string LastName { get; set; } = null!;
     public string FirstMidName { get; set; } = null!;
+    [DataType(DataType.Date)]
     public DateTime EnrollmentDate { get; set; }
 }
