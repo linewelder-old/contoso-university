595bccca31359912f38a3494c7cc403c18833ba2
linewelder <redalerm@gmail.com>
2023-08-27 10:57:48 +0200

    Add attributes to Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,14 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models;
 
 public class Student
 {
     public int ID { get; set; }
+
+    [Display(Name = "Last Name")]
+    [Required]
+    [StringLength(50)]
     public string LastName { get; set; } = null!;
+
+    [Display(Name = "First Name")]
+    [Column("FirstName")]
+    [Required]
+    [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
     public string FirstMidName { get; set; } = null!;
+
+    [Display(Name = "Enrollment Date")]
     [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
     public DateTime EnrollmentDate { get; set; }
+
+    [Display(Name = "Full Name")]
+    public string FullName => $"{LastName}, {FirstMidName}";
 
     public ICollection<Enrollment>? Enrollments { get; set; } = null;
 }
