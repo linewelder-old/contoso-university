7784a7c75db88db95144baf5827c8abe640252b5
linewelder <redalerm@gmail.com>
2023-09-17 16:40:31 +0200

    Use ModelState to report DbUpdateException in Students/Edit

--- Pages/Students/Edit.cshtml.cs
+++ Pages/Students/Edit.cshtml.cs
@@ -9,29 +9,21 @@ namespace ContosoUniversity.Pages.Students;
 public class EditModel : PageModel
 {
     private readonly SchoolContext _context;
-    private readonly ILogger<EditModel> _logger;
 
-    public EditModel(SchoolContext context, ILogger<EditModel> logger)
+    public EditModel(SchoolContext context)
     {
         _context = context;
-        _logger = logger;
     }
 
     [BindProperty]
     public Student Student { get; set; } = default!;
-    public string? ErrorMessage { get; set; }
 
-    public async Task<IActionResult> OnGetAsync(int id, bool? saveChangesError)
+    public async Task<IActionResult> OnGetAsync(int id)
     {
         var student = await _context.Students.FindAsync(id);
         if (student is null)
         {
             return NotFound();
-        }
-
-        if (saveChangesError.GetValueOrDefault())
-        {
-            ErrorMessage = $"Update {id} failed. Try again.";
         }
 
         Student = student;
@@ -60,11 +52,10 @@ public class EditModel : PageModel
             await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
-        catch (DbUpdateException ex)
+        catch (DbUpdateException)
         {
-            _logger.LogError(ex, "Edit student {} failed.", id);
-            return RedirectToAction("./Edit",
-                new { id, saveChangesError = true });
+            ModelState.AddModelError("", "Failed to save changes. Try again.");
+            return Page();
         }
     }
 }
